f5511a943f5a1ceb46a862e175fa1ab3fad27a55
Phil Chaplin <pchapl1@gmail.com>
2022-01-24 19:51:53 -0800

    first commit

---
+++ session_1/main.js
@@ -0,0 +1,26 @@
+let thornton = 'billy bob thornton'
+let title = 'Bad Santa'
+let movieYear= '2004'
+let description1 = 'boring stuff'
+let candy = 'jelly beans'
+let favFood = 'Arrachera'
+let commonFood = 'tacos'
+let number1 = 34567
+let color1 = 'yellow'
+let color2 = 'cerulean blue'
+let show1 = 'watchmen'  //11
+let show2 = 'Friends from College'
+let show3 = 'Mr. Robot'
+let show4 = 'The Office'
+let secretAgent = 'Michael Scarn'
+let shoeBrand = 'Nike'
+let theGoat = 'Michael Jordan'
+let classTime = '6:00'
+let num3 = 89
+let lastVar = 'Finally done'
+
+
+
+
+// myString = `I will be a ${jobtitle} in ${geoLocation} and married to ${parntersName} with ${numChildren} kids.`
+document.write(`${thornton} , ${title} , ${movieYear} , ${description1} , ${candy} , ${favFood} , ${commonFood} , ${number1} , ${color1} , ${color2} , ${show1} , ${show2} , ${show3} , ${show4} , ${secretAgent} , ${shoeBrand} , ${theGoat} , ${classTime} , ${num3} , ${lastVar} ,`)
